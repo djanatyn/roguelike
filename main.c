96468db64d19ace2f0ddf1e1eff6bc13ef25b7fb
djanatyn <djanatyn@gmail.com>
2011-03-13 22:25:23 -0400

    fixed step counter

--- main.c
+++ main.c
@@ -2,35 +2,41 @@
 int x = 2;
 int y = 2;
 
-void player_walk(dir)	// now includes collision check!
+int player_walk(dir)	// now includes collision check!
 {
+	int error = 1;
     switch(dir)
     {  
         case 106:		// down
             if(y < 21 && y >= 2)
 			{
 				y = y + 1;
+				error = 0;
 			}
             break;
         case 107:		// up
             if(y <= 21  && y > 2)
 			{
 				y = y - 1;
+				error = 0;
 			}
             break;
         case 104:		// left
             if(x <= 77 && x > 2)
 			{
 				x = x - 1;
+				error = 0;
 			}
             break;
         case 108:		// right
             if(x < 77 && x >= 2)
 			{
 				x = x + 1;
+				error = 0;
 			}
             break;
     }
+	return error;
 }
 
 int main()
@@ -49,8 +55,10 @@ int main()
         refresh();
         ch = getch();
         clear();
-        player_walk(ch);
-        steps = steps + 1;
+        if(player_walk(ch) == 0)
+		{
+			steps = steps + 1;
+		}
     }
 
     return 0;
