8cc09e0929d9c8f4b924c6fa884eee0a207af3c9
djanatyn <djanatyn@gmail.com>
2011-03-13 22:15:06 -0400

    added collision detection

--- main.c
+++ main.c
@@ -1,29 +1,41 @@
 #include <ncurses.h>
-int x = 0;
-int y = 0;
+int x = 2;
+int y = 2;
 
-void player_walk(dir)
+void player_walk(dir)	// now includes collision check!
 {
     switch(dir)
     {  
-        case 106:
-            y = y + 1;
+        case 106:		// down
+            if(y < 21 && y >= 2)
+			{
+				y = y + 1;
+			}
             break;
-        case 107:
-            y = y - 1;
+        case 107:		// up
+            if(y <= 21  && y > 2)
+			{
+				y = y - 1;
+			}
             break;
-        case 104:
-            x = x - 1;
+        case 104:		// left
+            if(x <= 77 && x > 2)
+			{
+				x = x - 1;
+			}
             break;
-        case 108:
-            x = x + 1;
+        case 108:		// right
+            if(x < 77 && x >= 2)
+			{
+				x = x + 1;
+			}
             break;
     }
 }
 
 int main()
 {
-    int ch;
+    char ch;
     int steps = 0;
     initscr();
     keypad(stdscr, TRUE);
